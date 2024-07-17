e71c2f28a762e1935d86fe71cbb7b25c8268f51b
Merve Balci <mervebalci090@gmail.com>
2024-07-16 21:04:28 -0300

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,12 @@
 function ListGroup() {
-  const cities = [
-    "Toronto",
-    "Havana",
-    "Montreal",
-    "Seoul",
-    "Frankfurt",
-    "Paris",
-  ];
+  let cities = ["Toronto", "Havana", "Montreal", "Seoul", "Frankfurt", "Paris"];
+
+  cities = [];
 
   return (
     <>
       <h1>My Vacation List</h1>
+      {cities.length === 0 && <p>No City Found</p>}
       <ul className="list-group">
         {cities.map((city) => (
           <li key={city}>{city}</li>
