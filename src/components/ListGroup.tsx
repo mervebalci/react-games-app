b25bb62a23fefb8e92b1174931fac8c22aa7ea0d
Merve Balci <mervebalci090@gmail.com>
2024-07-18 15:23:58 -0300

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,22 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let cities = ["Toronto", "Havana", "Montreal", "Seoul", "Frankfurt", "Paris"];
 
-  cities = [];
+  // Event handler
+  function handleClick(event: MouseEvent) {
+    console.log(event);
+  }
 
   return (
     <>
       <h1>My Vacation List</h1>
       {cities.length === 0 && <p>No City Found</p>}
       <ul className="list-group">
-        {cities.map((city) => (
-          <li key={city}>{city}</li>
+        {cities.map((city, index) => (
+          <li className="list-group-item" key={city} onClick={handleClick}>
+            {city}
+          </li>
         ))}
       </ul>
     </>
