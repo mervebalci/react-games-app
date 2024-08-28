72f09b011a60940b0235e58b73f971edb164b347
Merve Balci <mervebalci090@gmail.com>
2024-08-28 16:58:57 -0300

    Understanding the Effect Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import ExpenseTrackerApp from "./expense-tracker/components/ExpenseTrackerApp";
+// useEffect( () => {} )
+// To execute a piece of code AFTER a component is rendered
+
+import { useEffect, useRef } from "react";
 
 export default function App() {
+  const ref = useRef<HTMLInputElement>(null);
+
+  // Function inside will be called after each render
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
+
   return (
     <div>
-      <ExpenseTrackerApp />
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
