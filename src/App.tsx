0d467e5fd1869186ec6209d8a80b6c4aecd6d0da
Merve Balci <mervebalci090@gmail.com>
2024-08-29 13:18:14 -0300

    Effect Clean Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 // useEffect( () => {} )
 // To execute a piece of code AFTER a component is rendered
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
+
+function connect() {
+  console.log("Connecting");
+}
+
+function disconnect() {
+  console.log("Disconnecting");
+}
 
 export default function App() {
   const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    connect();
+
+    // Clean up function is optionally returned. It should stop or undo whatever the effect was doing.
+    return () => disconnect();
+  });
 
   return (
     <div>
