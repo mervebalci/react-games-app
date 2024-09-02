a2fc0a8ab0e12aac166393b8c6b58a601343219c
Merve Balci <mervebalci090@gmail.com>
2024-09-02 18:08:10 -0300

    Deleting Data

--- src/App.tsx
+++ src/App.tsx
@@ -36,13 +36,36 @@ export default function App() {
     return () => controller.abort();
   }, []);
 
+  function deleteUser(user: User) {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("http://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  }
+
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
