d540302b70905f916d20ee3f761d9d508f85f9a1
Merve Balci <mervebalci090@gmail.com>
2024-09-11 10:52:57 -0300

    Installing Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+export default function App() {
+  return (
+    <div>
+      <Button colorScheme="blue">Click</Button>
+    </div>
+  );
+}
