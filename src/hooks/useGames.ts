d97ff550fb75b84dea892bc0828509a126797f3b
Merve Balci <mervebalci090@gmail.com>
2024-09-15 15:24:39 -0300

    Creating a custom hook to fetch the games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+export default function useGames() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  // Using the Effect hook to send a fetch request to the backend
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("./games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)});
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
