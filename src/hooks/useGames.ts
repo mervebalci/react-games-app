6de96f15ced0c8d12283e2d737f5c3fc003293fb
Merve Balci <mervebalci090@gmail.com>
2024-09-19 10:47:27 -0300

    Improving user experience with loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -26,20 +26,27 @@ interface FetchGamesResponse {
 export default function useGames() {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   // Using the Effect hook to send a fetch request to the backend
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("./games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false)
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message);
+        setLoading(false)
+      });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
